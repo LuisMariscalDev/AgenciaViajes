9f6bb8c05cb1add79121c2a0c465cf5b5b4ff5de
Luis Mariscal <luisferdev16@gmail.com>
2024-01-21 14:44:03 -0600

    Routing

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,27 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => { // req: lo que enviamos, res: lo que express nos responde
-    res.render('Inicio')
+    res.render('inicio', {
+        pagina : 'Inicio'
+    });
 });
 
 router.get('/nosotros', (req, res) => {
-    res.render('Nosotros')
-})
+    res.render('nosotros', {
+        pagina : 'Nosotros'
+    });
+});
+
+router.get('/viajes', (req, res) => {
+    res.render('viajes', {
+        pagina : 'Viajes'
+    });
+});
+
+router.get('/testimoniales', (req, res) => {
+    res.render('testimoniales', {
+        pagina : 'Testimoniales'
+    });
+});
 
 export default router;
